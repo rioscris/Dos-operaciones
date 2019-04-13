ba741af5b9473376124c59055a444e2ed0acb84f
rioscris <rios7563@gmail.com>
2019-04-13 19:42:04 -0300

    Primer planteo
    Planteo inicial de clases y metodos para resolver el problema.

---
+++ src/codegolf/Operacion.java
@@ -0,0 +1,18 @@
+package codegolf;
+
+public class Operacion {
+	
+	public int sumar_1(int n){
+		return n+1;
+	}
+	
+	public int multiplicar_por_2(int n) {
+		return n*2;
+	}
+	
+	public int operaciones(int n) {
+		
+		return 1;
+	}
+	
+}
