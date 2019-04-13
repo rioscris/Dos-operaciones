eaf4400a60042393e34a1bf34612f4d218b99986
rioscris <rios7563@gmail.com>
2019-04-13 20:50:58 -0300

    Testing
    Pruebas realizadas exitosamente para:
    n = 1 - una sola operacion
    n = 5 - con n impar
    n = 20 - con n par
    
    Se puede considerar método funcionando correctamente.

--- src/Pruebas/OperacionesPruebas.java
+++ src/Pruebas/OperacionesPruebas.java
@@ -17,9 +17,39 @@ public class OperacionesPruebas {
 		o = new Operacion();
 	}
 	
+	/*
+	 * 1: 0+1 = 1
+	 */
 	@Test
 	public void queSeOperaUnaSolaVez() {
 		assertEquals(o.operaciones(1), 1);
 	}
 	
+	/*
+	 * 1: 0+1 = 1
+	 * 2: 1*2 = 2
+	 * 3: 2*2 = 4
+	 * 4: 4+1 = 5
+	 */
+	@Test
+	public void queConsideraNumerosImpares() {
+		assertEquals(o.operaciones(5), 4);
+	}
+	
+	
+	/*
+	 * 1: 0+1 = 1
+	 * 2: 1*2 = 2
+	 * 3: 2*2 = 4
+	 * 4: 4*2 = 8
+	 * 5: 8*2 = 16
+	 * 6: 16+1 = 17
+	 * 7: 17+1 = 18
+	 * 8: 18+1 = 19
+	 * 9: 19+1 = 20
+	 */
+	@Test
+	public void queConsideraNumerosPares() {
+		assertEquals(o.operaciones(20), 9);
+	}
 }
